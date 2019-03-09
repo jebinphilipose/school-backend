ec65166a533303eadc05a56b24c04aa00b8ebdd0
Jebin Philipose <jebinphilip24@gmail.com>
2019-03-09 13:03:18 +0530

    Create School API

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import School
+from .serializers import SchoolSerializer
+
 
 # Create your views here.
+class SchoolListView(generics.ListAPIView):
+    """
+    GET api/v1/schools/
+    """
+    queryset = School.objects.all()
+    serializer_class = SchoolSerializer
